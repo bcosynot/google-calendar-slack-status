3e9bd24462823ca9f6564d517648c8fc2bcd54fe
Vivek Ranjan <bcosynot@users.noreply.github.com>
2018-05-19 14:18:31 -0500

    Using status_emoji to set emoji

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ app.post('/', (req, res, next) => {
   slack.users.profile.set({
     token: process.env.SLACK_TOKEN,
     profile: JSON.stringify({
-      "status_text": `📅 ${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`
+      "status_text": `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`,
+      "status_emoji": `:calendar:`
     })
   });
   res.status(200);
